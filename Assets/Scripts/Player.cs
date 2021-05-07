7a2d935bf9cfa8ac6f35dbbd17794fa2a9083c33
Eliza Milburn <61363431+ERmilburn02@users.noreply.github.com>
2021-05-07 10:52:12 +0100

    Start work on scripts

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿public static class Player
+{
+    public static int Night { get; set; }
+    public static int Power { get; set; }
+    public static bool LeftDoorClosed { get; set; }
+    public static bool RightDoorClosed { get; set; }
+    public static bool CameraUp { get; set; }
+
+    public static bool Rev { get; set; }
+    public static bool Midna { get; set; }
+    public static bool Tucker { get; set; }
+
+    public static int DanDifficulty { get; set; }
+    public static int JerikaDifficulty { get; set; }
+    public static int AlyssaDifficulty { get; set; }
+    public static int BrigetteDifficulty { get; set; }
+}
