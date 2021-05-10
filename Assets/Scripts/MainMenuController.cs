2ca21b320b139025ed9d71e82a36b4e51445bd8c
Eliza Milburn <61363431+ERmilburn02@users.noreply.github.com>
2021-05-10 20:46:48 +0100

    Work on main menu UI

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
+    public TMP_Text CustomDanDifficulty;
+    public TMP_Text CustomJerikaDifficulty;
+    public TMP_Text CustomAlyssaDifficulty;
+    public TMP_Text CustomBrigetteDifficulty;
+    public Toggle CustomRev;
+    public Toggle CustomMidna;
+    public Toggle CustomTucker;
+
     [ContextMenu("Continue")]
     public void ContinueStory()
     {
         if (Player.SAVE_CurrentNight == -1)
         {
-            // No Save, start from Night 1
-            GameManager.SetupNight1();
-            SceneManager.LoadSceneAsync(2);
+            NewGame();
+        }
+        if (Player.SAVE_CurrentNight == 1)
+        {
+            NewGame();
         }
     }
 
@@ -20,6 +32,14 @@ public class MainMenuController : MonoBehaviour
     public void NewGame()
     {
         GameManager.SetupNight1();
+        SceneManager.LoadSceneAsync(2);
+    }
+
+
+    [ContextMenu("CustomNight")]
+    public void LaunchCustomNight()
+    {
+        GameManager.SetupNight(int.Parse(CustomDanDifficulty.text), int.Parse(CustomJerikaDifficulty.text), int.Parse(CustomAlyssaDifficulty.text), int.Parse(CustomBrigetteDifficulty.text), CustomRev.isOn, CustomMidna.isOn, CustomTucker.isOn, 7, 100, 12);
         SceneManager.LoadSceneAsync(2);
     }
 }
