c7fb501abae135a97e60b6ca251607131249b4d5
Eliza Milburn <61363431+ERmilburn02@users.noreply.github.com>
2021-05-11 14:37:10 +0100

    organisation & work on cameras

---
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public static CameraManager instance;
+
+    public RenderTexture HallwayCloseRT;
+    public RenderTexture HallwayFarRT;
+    public RenderTexture HellRT;
+    public RenderTexture KitchenRT;
+    public RenderTexture RecRoomRT;
+
+    public Dictionary<Room, RenderTexture> cams;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        instance = this;
+
+        cams = new Dictionary<Room, RenderTexture>()
+        {
+            {Room.HallwayClose, HallwayCloseRT},
+            {Room.HallwayFar, HallwayFarRT},
+            {Room.Hell, HellRT},
+            {Room.Kitchen, KitchenRT},
+            {Room.RecRoom, RecRoomRT}
+        };
+    }
+}
---
+++ Assets/Scripts/UI/CameraOpener.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CameraOpener : MonoBehaviour, IPointerEnterHandler
+{
+    public GameObject camDisplay;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        camDisplay.SetActive(!camDisplay.activeSelf);
+    }
+
+    // /// <summary>
+    // /// Called when the mouse enters the GUIElement or Collider.
+    // /// </summary>
+    // void OnMouseEnter()
+    // {
+    //     camDisplay.SetActive(!isOpen);
+    //     isOpen = !isOpen;
+    // }
+}
