e23062dcc369cee108d69b4572e3d18e5f330623
Eliza Milburn <61363431+ERmilburn02@users.noreply.github.com>
2021-05-17 21:10:56 +0100

    get UB builds working

---
+++ Assets/SuperUnityBuild/BuildConstants.cs
@@ -0,0 +1,42 @@
+// This file is auto-generated. Do not modify or move this file.
+
+public static class BuildConstants
+{
+    public enum ReleaseType
+    {
+        None,
+        Debug,
+        Release,
+    }
+
+    public enum Platform
+    {
+        None,
+        PC,
+        macOS,
+        Linux,
+        WebGL,
+    }
+
+    public enum Architecture
+    {
+        None,
+        Windows_x86,
+        macOS,
+        Linux_x64,
+        WebGL,
+    }
+
+    public enum Distribution
+    {
+        None,
+    }
+
+    public static readonly System.DateTime buildDate = new System.DateTime(637568728690955346);
+    public const string version = "0.1";
+    public const ReleaseType releaseType = ReleaseType.Debug;
+    public const Platform platform = Platform.PC;
+    public const Architecture architecture = Architecture.Windows_x86;
+    public const Distribution distribution = Distribution.None;
+}
+
