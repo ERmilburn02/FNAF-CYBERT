43df14a862b4e61188fdcaf990605b91b5bb788b
Eliza Milburn <61363431+ERmilburn02@users.noreply.github.com>
2021-05-17 10:46:34 +0100

    AI difficulty settings & Start despawn tucker

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -42,4 +42,32 @@ public class MainMenuController : MonoBehaviour
         GameManager.SetupNight(int.Parse(CustomDanDifficulty.text), int.Parse(CustomJerikaDifficulty.text), int.Parse(CustomAlyssaDifficulty.text), int.Parse(CustomBrigetteDifficulty.text), CustomRev.isOn, CustomMidna.isOn, CustomTucker.isOn, 7, 100, 12);
         SceneManager.LoadSceneAsync(2);
     }
+
+    public void DanAIChange(int _value)
+    {
+        CustomDanDifficulty.text = (int.Parse(CustomDanDifficulty.text) + _value).ToString();
+        if (CustomDanDifficulty.text == "21") { CustomDanDifficulty.text = "20"; }
+        if (CustomDanDifficulty.text == "-1") { CustomDanDifficulty.text = "0"; }
+    }
+
+    public void JerikaAIChange(int _value)
+    {
+        CustomJerikaDifficulty.text = (int.Parse(CustomJerikaDifficulty.text) + _value).ToString();
+        if (CustomJerikaDifficulty.text == "21") { CustomJerikaDifficulty.text = "20"; }
+        if (CustomJerikaDifficulty.text == "-1") { CustomJerikaDifficulty.text = "0"; }
+    }
+
+    public void AlyssaAIChange(int _value)
+    {
+        CustomAlyssaDifficulty.text = (int.Parse(CustomAlyssaDifficulty.text) + _value).ToString();
+        if (CustomAlyssaDifficulty.text == "21") { CustomAlyssaDifficulty.text = "20"; }
+        if (CustomAlyssaDifficulty.text == "-1") { CustomAlyssaDifficulty.text = "0"; }
+    }
+
+    public void BrigetteAIChange(int _value)
+    {
+        CustomBrigetteDifficulty.text = (int.Parse(CustomBrigetteDifficulty.text) + _value).ToString();
+        if (CustomBrigetteDifficulty.text == "21") { CustomBrigetteDifficulty.text = "20"; }
+        if (CustomBrigetteDifficulty.text == "-1") { CustomBrigetteDifficulty.text = "0"; }
+    }
 }
